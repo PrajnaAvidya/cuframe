f780633f24fe82518f814fe390e09f8fb9d1e5ca
Rafiq <rafiq@premji.me>
2025-12-05 18:28:44 -0700

    vendor nv codec sdk headers, find ffmpeg in cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
+#include <cuda.h>
 #include <cuda_runtime.h>
+#include <cuviddec.h>
+#include <nvcuvid.h>
+
+extern "C" {
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/avutil.h>
+}
+
 #include <cstdio>
 
 int main() {
@@ -16,5 +26,25 @@ int main() {
                i, props.name, props.major, props.minor,
                props.totalGlobalMem / (1024.0 * 1024.0 * 1024.0));
     }
+
+    printf("ffmpeg: %s\n", av_version_info());
+
+    // init driver api and force context creation (required before cuvid calls)
+    cuInit(0);
+    cudaFree(0);
+
+    // verify nvdec symbol resolves
+    CUVIDDECODECAPS caps = {};
+    caps.eCodecType = cudaVideoCodec_H264;
+    caps.eChromaFormat = cudaVideoChromaFormat_420;
+    caps.nBitDepthMinus8 = 0;
+    CUresult res = cuvidGetDecoderCaps(&caps);
+    if (res == CUDA_SUCCESS && caps.bIsSupported) {
+        printf("nvdec: h264 decode supported, max %dx%d\n",
+               caps.nMaxWidth, caps.nMaxHeight);
+    } else {
+        printf("nvdec: cuvidGetDecoderCaps returned %d\n", res);
+    }
+
     return 0;
 }
